c533eecc259c04b4fb8deaa0f456378199e96ace
Ziqiang Cheng <ziqiangcheng@staff-210-064.wireless.adelaide.edu.au>
2019-12-16 12:23:06 +1030

    carousal finished

--- project_valkyrie/lib/app/base_view.dart
+++ project_valkyrie/lib/app/base_view.dart
@@ -1,38 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class BaseView extends StatelessWidget {
   // final _selectedIndex = 1;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Parcel',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Calender',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: More',
-      style: optionStyle,
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
+    print('---------rebuilding-------------');
     final selected = Provider.of<ValueNotifier<int>>(context);
+
+    String pageTitle;
+    switch (selected.value) {
+                  case 0:
+                    pageTitle = 'Home';
+                    break;
+                  case 1:
+                    pageTitle = 'Parcel'; 
+                    break;
+                  case 2:
+                    pageTitle = 'Calendar';
+                    break;
+                  default:
+                    pageTitle = pageTitle;
+                    break;
+                }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Base View'),
+        title: Text(pageTitle),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(selected.value),
+      body: Container(
+        margin: const EdgeInsets.only(top: 50.0),
+        child: CarouselSlider(
+          height: 200.0,
+          initialPage: 0,
+          autoPlay: true,
+          enlargeCenterPage: true,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          pauseAutoPlayOnTouch: Duration(seconds: 5),
+          items: [1, 2, 3].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                Color color;
+                switch (selected.value) {
+                  case 0:
+                    color = Colors.green[500];
+                    break;
+                  case 1:
+                    color = Colors.amber;
+                    break;
+                  case 2:
+                    color = Colors.red;
+                    break;
+                  default:
+                    color = Colors.blue;
+                    break;
+                }
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                      color: color,
+                    ),
+                    child: Text(
+                      'text ${selected.value}',
+                      style: TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -69,7 +109,7 @@ class BaseView extends StatelessWidget {
         unselectedItemColor: Colors.grey,
         unselectedIconTheme:
             IconThemeData(color: Colors.grey, opacity: 1.0, size: 25.0),
-        onTap: (_index) => selected.value = _index,
+        onTap: (_index) => _index != 3 ? selected.value = _index : null,
       ),
     );
   }
