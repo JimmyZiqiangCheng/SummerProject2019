3252beb6994219a1efb0e86a088167c3078895ce
Ziqiang Cheng <ziqiangcheng@staff-210-197.wireless.adelaide.edu.au>
2020-01-06 11:17:13 +1030

    refactored carousel and added dot indicator

--- project_valkyrie/lib/ui/shared_widgets/my_calendar.dart
+++ project_valkyrie/lib/ui/shared_widgets/my_calendar.dart
@@ -22,11 +22,8 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin{
     super.initState();
     final _selectedDay = DateTime.now();
 
+    // only show events within a certain range from the current date
     _events = {
-      _selectedDay.subtract(Duration(days: 30)): ['Event A0', 'Event B0', 'Event C0'],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): ['Event A2', 'Event B2', 'Event C2', 'Event D2'],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
       _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
       _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
       _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
@@ -34,12 +31,9 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin{
       _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
       _selectedDay.add(Duration(days: 3)): Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
       _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): ['Event A12', 'Event B12', 'Event C12', 'Event D12'],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): ['Event A14', 'Event B14', 'Event C14'],
     };
 
+    //_selectedEvents = _events[_selectedDay] ?? [];
     _selectedEvents = _events[_selectedDay] ?? [];
     _calendarController = CalendarController();
 
@@ -59,14 +53,14 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin{
   }
 
   void _onDaySelected(DateTime day, List events) {
-    print('CALLBACK: _onDaySelected');
+    //print('CALLBACK: _onDaySelected');
     setState(() {
       _selectedEvents = events;
     });
   }
 
   void _onVisibleDaysChanged(DateTime first, DateTime last, CalendarFormat format) {
-    print('CALLBACK: _onVisibleDaysChanged');
+    //print('CALLBACK: _onVisibleDaysChanged');
   }
 
   @override
@@ -91,15 +85,11 @@ class CalendarState extends State<Calendar> with TickerProviderStateMixin{
       availableGestures: AvailableGestures.all,
       availableCalendarFormats: const {
         CalendarFormat.month: '',
-        CalendarFormat.week: '',
       },
       calendarStyle: CalendarStyle(
         outsideDaysVisible: false,
-        weekendStyle: TextStyle().copyWith(color: Colors.red),
-        holidayStyle: TextStyle().copyWith(color: Colors.red),
       ),
       daysOfWeekStyle: DaysOfWeekStyle(
-        weekendStyle: TextStyle().copyWith(color: Colors.red),
       ),
       headerStyle: HeaderStyle(
         centerHeaderTitle: true,
--- project_valkyrie/lib/ui/shared_widgets/my_carousel.dart
+++ project_valkyrie/lib/ui/shared_widgets/my_carousel.dart
@@ -4,61 +4,100 @@ import 'package:transparent_image/transparent_image.dart';
 import 'package:project_valkyrie/ui/styles/text_styles.dart';
 import 'package:project_valkyrie/ui/styles/ui_helper.dart';
 
-class Carousel extends StatelessWidget {
+class Carousel extends StatefulWidget {
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int _current = 0;
+  double _carouselHeight = 400.0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 50.0),
-      child: CarouselSlider(
-        viewportFraction: 0.9,
-        height: 400.0,
-        initialPage: 0,
-        autoPlay: true,
-        enlargeCenterPage: true,
-        autoPlayInterval: Duration(seconds: 3),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        pauseAutoPlayOnTouch: Duration(seconds: 5),
-        items: [1, 2, 3].map((i) {
-          return Builder(
-            builder: (BuildContext context) {
-              return Column(
-                children: <Widget>[
-                  Card(
-                    //color: Colors.blueGrey[200],
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(UIHelper.circularAngle)),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(UIHelper.circularAngle),
-                        boxShadow:[UIHelper.customBoxShadow],
-                      ),
-                      height: 250,
-                      width: MediaQuery.of(context).size.width,
-                      child: ClipRRect(
-                        borderRadius: new BorderRadius.circular(UIHelper.circularAngle),
-                        child: FadeInImage.memoryNetwork(
-                          placeholder: kTransparentImage,
-                          image: 'https://picsum.photos/300?image=${i + 99}',
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
+      child: Stack(
+        children: <Widget>[
+          CarouselSlider(
+              viewportFraction: 0.9,
+              height: _carouselHeight,
+              initialPage: 0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              pauseAutoPlayOnTouch: Duration(seconds: 5),
+              onPageChanged: (index) {
+                setState(() {
+                  _current = index;
+                });
+              },
+              items: _carouselItemList
+            ),
+          Positioned(
+            top: _carouselHeight - 70.0,
+            left: 0.0,
+            right: 0.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [0,1,2].map((i) {
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == i ? Color.fromRGBO(0, 0, 0, 0.9) : Color.fromRGBO(0, 0, 0, 0.4)
                   ),
-                  UIHelper.verticalSpaceLarge,
-                  Container(
-                    child: Text(
-                      'Event: $i',
-                      style:
-                          TextStyles.subHeaderStyle(),
-                    ),
-                  ),
-                ],
-              );
-            },
-          );
-        }).toList(),
+                );
+              }).toList(),
+            )
+          )
+        ],
       ),
     );
   }
+
+  List _carouselItemList = [0, 1, 2].map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Column(
+          children: <Widget>[
+            Card(
+              //color: Colors.blueGrey[200],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(UIHelper.circularAngle)),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(UIHelper.circularAngle),
+                  boxShadow: [UIHelper.customBoxShadow],
+                ),
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: ClipRRect(
+                  borderRadius:
+                      new BorderRadius.circular(UIHelper.circularAngle),
+                  child: FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: 'https://picsum.photos/300?image=${i + 99}',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            UIHelper.verticalSpaceLarge,
+            Container(
+              child: Text(
+                'Event: $i',
+                style: TextStyles.subHeaderStyle(),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }).toList();
 }
